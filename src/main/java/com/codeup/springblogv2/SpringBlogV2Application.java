4fbc2d6b753412a15f2cf8422e27500058fdc897
Johnny <mrtnz.jn@gmail.com>
2023-06-05 12:26:18 -0500

    modified gitignore

---
+++ src/main/java/com/codeup/springblogv2/SpringBlogV2Application.java
@@ -0,0 +1,13 @@
+package com.codeup.springblogv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBlogV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBlogV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/codeup/springblogv2/SpringBlogV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeup.springblogv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBlogV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
